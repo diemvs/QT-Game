7e84c9bc190896e59e99172b59e6e8e997c54518
Diemvs <sataev.dima@mail.ru>
2019-11-01 00:05:05 +0300

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -1,3 +1,6 @@
+#include "enemy.h"
+
+#include <QTimer>
 #include <QDebug>
 #include <QGraphicsScene>
 #include <stdlib.h>
@@ -10,5 +13,41 @@ Enemy::Enemy(int x, int y)
     setPos(x, y);
     //setRect(0, 0, 20,20);
     //setPixmap(QPixmap(":/images/im.jpg")); //":/images/im.jpg"
+    setRect(0, 0, 30, 30);
 }
 
+void Enemy::move()
+{
+    //int random_number = rand() % 5;
+    int random_number = 0;
+    switch(random_number) {
+        case 1:
+        setPos(x(), y()+5);
+        if (pos().y() + rect().height() < 0 ){
+            scene()->removeItem(this);
+            delete this;
+        }
+            break;
+        case 2:
+        setPos(x(), y()-5);
+        if (pos().y() + rect().height() < 0){
+             scene()->removeItem(this);
+            delete this;
+        }
+            break;
+        case 3:
+        setPos(x()+5, y());
+        if (pos().y() + rect().height() < 0){
+             scene()->removeItem(this);
+            delete this;
+        }
+             break;
+         case 4:
+                 setPos(x()-5, y());
+                 if (pos().y() + rect().height() < 0){
+                     scene()->removeItem(this);
+                     delete this;
+                 }
+                     break;
+    }
+}
